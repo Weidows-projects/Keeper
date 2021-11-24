5469e07092157b5653fe57414111d3a306a2a74d
Weidows <utsuko27@qq.com>
2021-11-24 15:13:49 +0800

    🥱 test.

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
  * @Author: Weidows
  * @Date: 2020-11-28 17:36:36
  * @LastEditors: Weidows
- * @LastEditTime: 2021-11-01 21:29:44
+ * @LastEditTime: 2021-11-24 15:13:05
  * @FilePath: \Keeper\README.md
  * @Description:
 -->
@@ -21,15 +21,18 @@
 - [x] 访问唤醒休眠的 LeanCloud 评论后台 (也可以是其他地址
 - [x] 获取`必应壁纸`,存储在[Bing](./Bing/)里面.
 - [x] 同步 github 仓库到 gitee.
+- [x] 调用 dailycheckin / automihoyobbs 定时任务
 
 - 进入 settings 配置 secret :
 
-  |    name     |                                                                               value (不会泄露,未填的项不会启用)                                                                               |
-  | :---------: | :-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------: |
-  | PUSH_EMAIL  |                                                                         github 推送邮箱 (默认使用 Github-action[bot])                                                                         |
-  |     URL     |                                                    需要唤醒的地址,支持多个如 `https://weidows.avosapps.us/comments https://www.baidu.com`                                                     |
-  |  GITEE_RSA  | 私钥文件内容; 公钥复制到 [用户设置](https://gitee.com/profile/sshkeys); 如何生成秘钥可以查看 [这篇文章](https://weidows.github.io/post/experience/SSH); 需要提前在 gitee 创建同名同邮箱的仓库 |
-  | GITEE_TOKEN |                                            用于镜像时自动创建不存在的仓库,Gitee 可以在[这里](https://gitee.com/profile/personal_access_tokens)找到                                            |
+  |         name         |                                                                               value (不会泄露,未填的项不会启用)                                                                               |
+  | :------------------: | :-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------: |
+  |      PUSH_EMAIL      |                                                                         github 推送邮箱 (默认使用 Github-action[bot])                                                                         |
+  |         URL          |                                                    需要唤醒的地址,支持多个如 `https://weidows.avosapps.us/comments https://www.baidu.com`                                                     |
+  |      GITEE_RSA       | 私钥文件内容; 公钥复制到 [用户设置](https://gitee.com/profile/sshkeys); 如何生成秘钥可以查看 [这篇文章](https://weidows.github.io/post/experience/SSH); 需要提前在 gitee 创建同名同邮箱的仓库 |
+  |     GITEE_TOKEN      |                                            用于镜像时自动创建不存在的仓库,Gitee 可以在[这里](https://gitee.com/profile/personal_access_tokens)找到                                            |
+  | DAILY_CHECKIN_CONFIG |                                                       [dailycheckin](https://github.com/Sitoi/dailycheckin) 的 config.json 配置文件内容                                                       |
+  | AUTOMIHOYOBBS_CONFIG |                                                     [automihoyobbs](https://github.com/Womsxd/AutoMihoyoBBS) 的 config.json 配置文件内容                                                      |
 
 ![分割线](https://cdn.jsdelivr.net/gh/Weidows/Images/img/divider.png)
 
@@ -43,8 +46,8 @@
 
   `机子有价,数据无价`,可参考我的备份仓库: [Weidows-projects/Programming-Configuration](https://github.com/Weidows-projects/Programming-Configuration)
 
-- [x] 各平台每日签到,交给 github-action 容易被查封,所以在本机手动跑.
-- [x] 用于管理开机启动/批量启动软件,aria2 后台启动
+- [x] 各平台每日签到,某些交给 github-action 容易被查封,所以在本机手动跑.
+- [x] 用于管理开机启动/批量启动软件,aria2 后台启动(最佳方案)
 
 ![分割线](https://cdn.jsdelivr.net/gh/Weidows/Images/img/divider.png)
 
@@ -53,3 +56,6 @@
 > [justjavac/auto-green](https://github.com/justjavac/auto-green) \
 > [mstf/bingdownload](https://gitee.com/mstf/bingdownload) \
 > [Yikun/hub-mirror-action](https://github.com/Yikun/hub-mirror-action/)
+
+> [Sitoi/dailycheckin](https://github.com/Sitoi/dailycheckin)\
+> [Womsxd/AutoMihoyoBBS](https://github.com/Womsxd/AutoMihoyoBBS)
--- utils.bat
+++ utils.bat
@@ -7,7 +7,9 @@
   chcp 65001
 
   @REM !!!!一定要注意等号'='前后不要加空格!!!!
-  set BACKUP_DIR=D:\Game\Github\Programming-Configuration
+  @REM 不设置的话,备份默认存放在keeper内的 Programming-Configuration, 路径支持含空格
+  @REM 例如: BACKUP_DIR=E:\OneDrive - Nima Company\Repo\Weidows-projects\Programming-Configuration
+  set BACKUP_DIR=
 
 
 
@@ -21,14 +23,12 @@
   @REM 清屏
     cls
 
-
   @REM 改色
     set /a a=%random%%%10
     color 0%a%
 
   @REM 初始化 choice
     set choice=-1
-
 
   echo                    .::::.
   echo                  .::::::::.
@@ -40,28 +40,29 @@
   echo             ..::::::::::::.
   echo           ``::::::::::::::::
   echo            ::::``:::::::::'        .:::.
-  echo           ::::'   ':::::'       .::::::::.(6)test
-  echo         .::::'    ::::::     .:::::::'::::. (5)dir
-  echo        .:::'     :::::::  .:::::::::' ':::::. (4)bilibili/miHoYo-helper
-  echo       .::'       ::::::.:::::::::'      ':::::. (3)devenv-starter
-  echo      .::'        :::::::::::::::'         ``::::. (2)boot-starter
-  echo  ...:::          :::::::::::::'              ``::. (1)backup
-  echo  ````':.          ':::::::::'                  ::::.. (0)exit
-  set /p choice=输入选项:           '.:::::'                    ':'```:..
+  echo           ::::'   ':::::'       .::::::::.(7)test / change color
+  echo         .::::'    ::::::     .:::::::'::::. (6)dir
+  echo        .:::'     :::::::  .:::::::::' ':::::. (5)daily-helper
+  echo       .::'       ::::::.:::::::::'      ':::::. (4)devenv-starter
+  echo      .::'        :::::::::::::::'         ``::::. (3)boot-starter
+  echo  ...:::          :::::::::::::'              ``::. (2)backup
+  echo  ````':.          ':::::::::'                  ::::.. (1)exit
+  echo 输入选项:           '.:::::'                    ':'```:..
+  CHOICE /C 1234567
   echo =============================================================================
 
 
 
-  if %choice%==0 exit
-  if %choice%==1 call :backup
-  if %choice%==2 call :boot-starter
-  if %choice%==3 call :devenv-starter
-  if %choice%==4 call :bilibili-helper
-  if %choice%==5 call :dir
-  if %choice%==6 call :test
+  if %errorlevel%==1 exit
+  if %errorlevel%==2 call :backup
+  if %errorlevel%==3 call :boot-starter
+  if %errorlevel%==4 call :devenv-starter
+  if %errorlevel%==5 call :daily-helper
+  if %errorlevel%==6 call :dir
+  if %errorlevel%==7 call :test
 
 
-  @REM 暂停-查看程序输出-自循环
+  @REM 暂停-查看程序输出-自循环; 视 goto 优先级过高只在 main 中用,其他的 只用 call
     pause & goto :circle
 goto :eof
 
@@ -74,24 +75,35 @@ goto :eof
 @REM 开机后设置备份,使用start是在新的终端同时进行的,call是按顺序依次
 @REM ==================================================================
 :backup
+  @REM 判断是否设置值
+    if not defined BACKUP_DIR (
+      echo 'BACKUP_DIR' not defined, use default path: %~dp0Programming-Configuration
+      set BACKUP_DIR=%~dp0Programming-Configuration
+    )
+    mkdir %BACKUP_DIR% & cd /d %BACKUP_DIR%
+
+
   @REM 备份 backup/ , mkdir 不会覆盖已存dir; 第一次cd有可能切换盘符,加上/d
-    mkdir %BACKUP_DIR%\backup & cd /d %BACKUP_DIR%\backup
+    mkdir backup & cd backup
 
     @REM 备份ssh 目录后都必须加个'\' (比如.ssh有可能是目录,也可能是文件,而.ssh\只可能是目录)
     xcopy %HOME%\.ssh\ .ssh\ /e/y/d
 
+    cd ..
+
 
   @REM 备份lists
-    mkdir %BACKUP_DIR%\lists & cd %BACKUP_DIR%\lists
+    mkdir lists & cd lists
 
     call xrepo scan > cpp\xrepo-scan.bak
 
     dir /b D:\Musics\Local > dir\dir-music.bak
-    dir /b D:\Software > dir\dir-software.bak
     dir /b E:\mystream > dir\dir-mystream.bak
+    dir /b D:\Software > dir\dir-software.bak
+    dir /b %HOME%\.vscode\extensions > dir\dir-.vscode.bak
 
-    call gh repo list > github\repo-list-Weidows.bak
-    call gh repo list weidows-projects > github\repo-list-weidows-projects.bak
+    call gh repo list > github\repolist-Weidows.bak
+    call gh repo list Weidows-projects > github\repolist-Weidows-projects.bak
 
     call npm -g list > node\npm-global.bak
     call yarn global list > node\yarn-global.bak
@@ -101,21 +113,35 @@ goto :eof
 
     call scoop list > scoop\scoop-apps.bak
     call scoop bucket list > scoop\scoop-buckets.bak
+    @REM 获取当前文件夹名称
+    @REM for /f "delims=" %%i in ("%cd%") do set folder=%%~ni
+    set currentPath=%cd%
+    for /d %%i in (%SCOOP%\buckets\*) do (
+      cd /d %%i
+      call git remote get-url origin >> %currentPath%\scoop\scoop-buckets.bak
+    )
+    cd /d %currentPath%
     call choco list -l > scoop\choco-list-local.bak
 
     @REM 重装系统/重装wallpaper engine,所有壁纸会木大,所以备份
     xcopy %SCOOP%\persist\steam\steamapps\common\wallpaper_engine\config.json .\wallpaper_engine\ /y/d
 
+    cd ..
+
 
   @REM 备份其他
-    mkdir %BACKUP_DIR%\others & cd %BACKUP_DIR%\others
+    mkdir others & cd others
+
     xcopy %windir%\System32\drivers\etc\hosts hosts\ /e/y/d
     xcopy %SCOOP%\persist\maven\conf\settings.xml maven\conf\ /e/y/d
     xcopy D:\Documents\PowerShell\Microsoft.PowerShell_profile.ps1 .\PowerShell\ /e/y/d
 
+    cd ..
+
 
   @REM 备份 ~\
-    mkdir %BACKUP_DIR%\user-config & cd %BACKUP_DIR%\user-config
+    mkdir user-config & cd user-config
+
     xcopy %HOME%\.conda\ .conda\ /e/y/d
     xcopy %HOME%\.config\ .config\ /e/y/d
     xcopy %HOME%\pip\ pip\ /e/y/d
@@ -124,8 +150,13 @@ goto :eof
     xcopy %HOME%\.yarnrc . /y/d
     xcopy %HOME%\.condarc . /y/d
     xcopy %HOME%\.gitconfig . /y/d
+
     @REM git-bash 样式
-      xcopy %HOME%\.minttyrc . /y/d
+    xcopy %HOME%\.minttyrc . /y/d
+    xcopy %HOME%\.vscode\projects.json .vscode\ /y/d
+
+    cd ..
+
 goto :eof
 
 
@@ -137,9 +168,9 @@ goto :eof
 @REM 开机启动软件
 @REM ==================================================================
 :boot-starter
-  start Rainmeter
-  start MouseInc
-  start %SCOOP%\apps\N0vaDesktop\current\N0vaDesktop.exe
+  start /b Rainmeter
+  start /b MouseInc
+  start /b n0vadesktop
 
   @REM aria2: 直接通过shell启动会被它占用,所以另开
   echo CreateObject("WScript.Shell").Run "aria2c --conf-path=D:\Game\Scoop\persist\aria2\conf",0 > aria2.vbs
@@ -147,7 +178,7 @@ goto :eof
   del aria2.vbs
 
   @REM 酷狗
-  start D:\Software\KGMusic\KuGou.exe
+  start /b D:\Software\KGMusic\KuGou.exe
 goto :eof
 
 
@@ -160,21 +191,21 @@ goto :eof
 @REM ==================================================================
 :devenv-starter
   @REM 文件管理
-  start xyplorerfree
+  start /b xyplorerfree
 
   @REM IDE
-  start code
-  start idea64.exe
+  start /b code
+  start /b idea64.exe
 
   @REM 浏览器
-  start microsoft-edge:
+  start /b microsoft-edge:
 
   @REM 通讯
-  start %SCOOP%\apps\TIM\current\Bin\TIM.exe
-  start %SCOOP%\apps\wechat\current\WeChat.exe
+  start /b %SCOOP%\apps\TIM\current\Bin\TIM.exe
+  start /b %SCOOP%\apps\wechat\current\WeChat.exe
 
   @REM 虚拟机
-  start vmware
+  start /b vmware
 goto :eof
 
 
@@ -183,19 +214,25 @@ goto :eof
 
 
 @REM ==================================================================
-@REM Bilibili/miHoYo-helper
+@REM scoop-update / Bilibili
 @REM ==================================================================
-:bilibili-helper
+:daily-helper
   @REM %~dp0 为脚本所在路径; %cd% 类似 pwd,当前路径
   cd /d %~dp0\local
 
-  call conda activate base
-  start /b python AutoMihoyoBBS/main.py
+  @REM log
+    for /l %%i in (1 1 5) do echo.>> log\tasks.log
+    set date =date /T
+    echo =================================%date%===========================>> log\tasks.log
 
-  cd BILIBILI-HELPER
-  start /b java -jar BILIBILI-HELPER.jar
-  cd ..
+  @REM scoop-update
+    call scoop update | tee -a log\tasks.log
 
+  @REM bilibili
+    cd BILIBILI-HELPER
+    call java -jar BILIBILI-HELPER.jar | tee -a ..\log\tasks.log
+    cd ..
+    rd /S/Q D:\tmp
 goto :eof
 
 
@@ -221,5 +258,14 @@ goto :eof
 @REM ==================================================================
 :test
   echo Testing...
+
+  @REM dailycheckin (cmd会由于Unicode报错)
+    @REM cd /d %~dp0\local
+    @REM call conda activate base
+    @REM start powershell dailycheckin --include ACFUN CLOUD189 MUSIC163 TIEBA
+
+  @REM 米游社
+    @REM call conda activate base
+    @REM call python AutoMihoyoBBS/main.py >> tasks.log
 
 goto :eof
