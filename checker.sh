c1d6c8c663d579d479ef5a504c6ac77f57928e0e
Weidows <utsuko27@qq.com>
2022-03-20 00:54:59 +0800

    🥳 完善.

--- checker.sh
+++ checker.sh
@@ -1,55 +1,30 @@
-mkdir ./tmp -p
+chmod +x ./runcheck.sh
 
-curl https://${1} -k -v -s -l 2> ./tmp/ca.info
+mkdir ./tmp/api -p
 
-cat ./tmp/ca.info | grep 'start date: ' > ./tmp/${1}.info
-cat ./tmp/ca.info | grep 'expire date: ' >> ./tmp/${1}.info
-cat ./tmp/ca.info | grep 'issuer: ' >> ./tmp/${1}.info
-cat ./tmp/ca.info | grep 'SSL certificate verify' >> ./tmp/${1}.info
-cat ./tmp/ca.info | grep 'subject: ' >> ./tmp/${1}.info
+for i in $@
+do
+    ./runcheck.sh ${i}
+done
 
-sed -i 's|\*  start date: ||g' ./tmp/${1}.info
-sed -i 's|\*  expire date: ||g' ./tmp/${1}.info
-sed -i 's|\*  issuer: ||g' ./tmp/${1}.info
-sed -i 's|\*  SSL certificate verify ||g' ./tmp/${1}.info
-sed -i 's|\*  subject: ||g' ./tmp/${1}.info
+echo '[' > ./tmp/api/ct.json
 
-start=$(sed -n '1p' ./tmp/${1}.info)
-expire=$(sed -n '2p' ./tmp/${1}.info)
-issuer=$(sed -n '3p' ./tmp/${1}.info)
-status=$(sed -n '4p' ./tmp/${1}.info)
-subject=$(sed -n '5p' ./tmp/${1}.info)
+for i in $@
+do
+    cat ./tmp/${i}.json >> ./tmp/api/ct.json
+done
 
-rm -f ./tmp/ca.info
-rm -f ./tmp/${1}.info
+sed -i '$d' ./tmp/api/ct.json
 
-DATE="$(echo $(date '+%Y-%m-%d %H:%M:%S'))"
+echo '}' >> ./tmp/api/ct.json
+echo ']' >> ./tmp/api/ct.json
 
-nowstamp="$(date -d "$DATE" +%s)"
-expirestamp="$(date -d "$expire" +%s)"
-expireday=`expr $[expirestamp-nowstamp] / 86400`
+sed -i ':label;N;s/\n/ /;b label' ./tmp/api/ct.json
 
-echo '{' > tmp/${1}.json
-echo '"domain": "'${1}'",' >> ./tmp/${1}.json
-echo '"subject": "'$subject'",' >> ./tmp/${1}.json
-echo '"start": "'$start'",' >> ./tmp/${1}.json
-echo '"expire": "'$expire'",' >> ./tmp/${1}.json
-echo '"issuer": "'$issuer'",' >> ./tmp/${1}.json
+sed -i "s|\" \"||g" ./tmp/api/ct.json
+sed -i "s|\"\: \"|\"\:\"|g" ./tmp/api/ct.json
+sed -i "s|\"\, \"|\"\,\"|g" ./tmp/api/ct.json
+sed -i "s|\" }, { \"|\"},{\"|g" ./tmp/api/ct.json
 
-if [ $expirestamp \< $nowstamp ]; then
-    echo '"status": "Expired",' >> ./tmp/${1}.json
-    echo '"statuscolor": "error",' >> ./tmp/${1}.json
-elif [ $expireday \< 10 ]; then
-    echo '"status": "Soon Expired",' >> ./tmp/${1}.json
-    echo '"statuscolor": "warning",' >> ./tmp/${1}.json
-elif [ $status = "ok." ]; then
-    echo '"status": "Valid",' >> ./tmp/${1}.json
-    echo '"statuscolor": "success",' >> ./tmp/${1}.json
-else
-    echo '"status": "Invalid",' >> ./tmp/${1}.json
-    echo '"statuscolor": "error",' >> ./tmp/${1}.json
-fi
-
-echo '"check": "'$DATE'",' >> ./tmp/${1}.json
-echo '"remain": "'$expireday'"' >> ./tmp/${1}.json
-echo '},' >> ./tmp/${1}.json
+cp -rf ./tmp/api/ct.json ./ct.json
+rm -rf ./tmp
